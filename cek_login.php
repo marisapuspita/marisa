32160f8f9d32aa95f7bf7d111c71ce17b987dfb4
marisapuspita <mpuspita606@gmail.com>
2021-05-28 21:25:44 -0700

    Untuk mengetahui apakah masyarakat sudah pernah login atau belum

--- cek_login.php
+++ cek_login.php
@@ -1,1 +1,26 @@
-cek_login.php
+<?php 
+include 'koneksi.php';
+$user=$_POST['username'];
+$pass=$_POST['password'];
+$sql=$mysqli -> query("select * from masyarakat where username='$user' and password='$pass' ");
+$cek=mysqli_num_rows($sql);
+
+	if ($cek>0)
+ 	{
+		$data=mysqli_fetch_array($sql);
+		session_start();
+		$_SESSION['nama']=$user;
+		$_SESSION['nik']=$data['nik'];
+		header('location:masyarakat.php');
+	}
+	else
+	{
+		?>
+		<script type="text/javascript">
+			alert ('Username Atau Password tidak ditemukan');
+			window.location="index.php";
+		</script>
+<?php
+	}
+
+?>
